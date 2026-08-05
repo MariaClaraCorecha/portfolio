80c6c034936e63248e19735d06b33eb786af5bd9
mariaclaracorecha <mariaclaracorecha@gmail.com>
2026-08-05 14:56:38 -0300

    feat: enhance footer component with new layout, styles, and email functionality

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
   readonly year = new Date().getFullYear();
+  readonly githubUrl = 'https://github.com/MariaClaraCorecha';
+  readonly email = 'mariacorecha18@gmail.com';
 }
