c767e73594026dcb1cf83071fc7156b1f2fadb2d
mariaclaracorecha <mariaclaracorecha@gmail.com>
2026-08-05 10:25:36 -0300

    feat: enhance header with navigation links and menu for better accessibility

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -2,10 +2,11 @@ import { Component, input } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
