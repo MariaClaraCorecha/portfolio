58a937fc20666293dd043a764b59e7e0868e99e9
mariaclaracorecha <mariaclaracorecha@gmail.com>
2026-08-05 11:47:32 -0300

    feat: enhance technologies component with additional tech categories and icons

--- src/app/technologies/technologies.ts
+++ src/app/technologies/technologies.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { getTechIcon } from '../shared/tech-icon';
 
 interface TechCategory {
   name: string;
@@ -8,14 +10,22 @@ interface TechCategory {
 
 @Component({
   selector: 'app-technologies',
-  imports: [MatChipsModule],
+  imports: [MatChipsModule, MatIconModule],
   templateUrl: './technologies.html',
   styleUrl: './technologies.scss',
 })
 export class Technologies {
   readonly categories: TechCategory[] = [
     { name: 'Frontend', items: ['Angular', 'TypeScript', 'Angular Material', 'HTML', 'SCSS'] },
-    { name: 'Backend', items: ['Node.js', 'NestJS', 'REST APIs'] },
-    { name: 'Ferramentas', items: ['Git', 'Docker', 'VS Code', 'Figma'] },
+    {
+      name: 'Backend',
+      items: ['PHP', 'Java','Laravel', 'JavaScript', 'Linux', 'REST APIs', 'MySQL', 'PostgreSQL', 'SQLite'],
+    },
+    {
+      name: 'Ferramentas',
+      items: ['Git', 'GitHub', 'Docker', 'VS Code', 'IntelliJ IDEA', 'Figma', 'Banco de Dados', 'Trello'],
+    },
   ];
+
+  readonly getTechIcon = getTechIcon;
 }
